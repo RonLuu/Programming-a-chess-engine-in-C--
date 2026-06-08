af9e7ec1afcb9dbaa51fbaaf1ee3d80b48ef0a2d
trieukhail <trieukhail@student.unimelb.edu.au>
2026-06-08 14:17:38 +0700

    Add addWhitePawnMove

--- movegen.cpp
+++ movegen.cpp
@@ -22,8 +22,31 @@ void addQuietMove(Board& board, int move, MOVELIST& moveList) {
     (moveList.moves)[index].score = 0;
     moveList.currentSize++;
 }
+// Add an extra rule for promotion
+void addWhitePawnMove(Board& board, int from, MOVELIST& moveList) {
+    int to = from + 10;
+
+    assert(isSqOnBoard(from));
+    assert(isSqOnBoard(to));
+
+    if (sqToRank[from] != RANK_7) {
+        int move = makeMove(from, to, EMPTY, EMPTY, 0);
+        addQuietMove(board, move, moveList);
+    } else {
+        int promoteQ = makeMove(from, to, EMPTY, wQ, 0);
+        int promoteR = makeMove(from, to, EMPTY, wR, 0);
+        int promoteN = makeMove(from, to, EMPTY, wN, 0);
+        int promoteB = makeMove(from, to, EMPTY, wB, 0);
+
+        addQuietMove(board, promoteQ, moveList);
+        addQuietMove(board, promoteR, moveList);
+        addQuietMove(board, promoteN, moveList);
+        addQuietMove(board, promoteB, moveList);
+    }
+}
 
 
 void generateAllMoves(Board& board, MOVELIST& moveList) {
+
     
 }
