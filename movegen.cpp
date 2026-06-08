83130412eca429690dd3ce2b9c3775e5ee5437ab
trieukhail <trieukhail@student.unimelb.edu.au>
2026-06-08 14:15:46 +0700

    Add addEnPassantMove function

--- movegen.cpp
+++ movegen.cpp
@@ -6,13 +6,19 @@ void addCaptureMove(Board& board, int move, MOVELIST& moveList) {
     (moveList.moves)[index].score = 0;
     moveList.currentSize++;
 }
-
+void addEnPassantMove(Board& board, int move, MOVELIST& moveList) {
+    int index = moveList.currentSize;
+    (moveList.moves)[index].move = move;
+    (moveList.moves)[index].score = 0;
+    moveList.currentSize++;
+}
 void addQuietMove(Board& board, int move, MOVELIST& moveList) {
     int index = moveList.currentSize;
     (moveList.moves)[index].move = move;
     (moveList.moves)[index].score = 0;
     moveList.currentSize++;
 }
+
 
 void generateAllMoves(Board& board, MOVELIST& moveList) {
     
