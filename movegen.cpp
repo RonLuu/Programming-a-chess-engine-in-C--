1f7d14829627fb8b26f89e2778b5034c90c6ca92
trieukhail <trieukhail@student.unimelb.edu.au>
2026-06-08 14:14:03 +0700

    Add addQuietMove function

--- movegen.cpp
+++ movegen.cpp
@@ -1,5 +1,12 @@
 #include "movegen.hpp"
 
+void addQuietMove(Board& board, int move, MOVELIST& moveList) {
+    int index = moveList.currentSize;
+    (moveList.moves)[index].move = move;
+    (moveList.moves)[index].score = 0;
+    moveList.currentSize++;
+}
+
 void generateAllMoves(Board& board, MOVELIST& moveList) {
     
 }
