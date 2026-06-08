c2821a2c4abd0d0c018fea82b3cdd4f483617ba5
trieukhail <trieukhail@student.unimelb.edu.au>
2026-06-08 14:53:13 +0700

    Add isAttackedByPawn

--- attack.cpp
+++ attack.cpp
@@ -1,8 +1,22 @@
 #include "attack.hpp"
 
+bool isAttackedByPawn(int sq, int side, Board& board) {
+    if (side == WHITE) {
+        if (board.squareToPiece[sq - 11] == wP || board.squareToPiece[sq - 9] == wP) {
+            return true;
+        }
+    } else {
+        if (board.squareToPiece[sq + 11] == bP || board.squareToPiece[sq + 9] == bP) {
+            return true;
+        }
+    }
+    return false;
+}
+
 bool isSqBeingAttacked(int sq, int side, Board &board) {
     assert(isSqOnBoard(sq));
     assert(isSideValid(side));
     assert(board.checkBoard());
 
+    return isAttackedByPawn(sq, side, board)
 }
