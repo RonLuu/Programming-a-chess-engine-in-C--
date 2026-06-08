04ff767670af886e7c0b2f18efe80161cb8eb88d
trieukhail <trieukhail@student.unimelb.edu.au>
2026-06-08 14:16:35 +0700

    Add makeMove expression

--- movegen.cpp
+++ movegen.cpp
@@ -1,5 +1,9 @@
 #include "movegen.hpp"
 
+constexpr int makeMove(int from, int to, int captured, int promoted, int FLAG) {
+    return from | (to << 7) | (captured << 14) | (promoted << 20) | FLAG;
+}
+
 void addCaptureMove(Board& board, int move, MOVELIST& moveList) {
     int index = moveList.currentSize;
     (moveList.moves)[index].move = move;
