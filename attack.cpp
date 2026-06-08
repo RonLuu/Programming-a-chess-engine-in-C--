f4f30aba99e014c6c993fb05b4c478edb9eae103
trieukhail <trieukhail@student.unimelb.edu.au>
2026-06-08 16:39:24 +0700

    Add isAttackedByRookQueen function

--- attack.cpp
+++ attack.cpp
@@ -1,8 +1,10 @@
 #include "attack.hpp"
 
 constexpr int numKnDir = 8;
+constexpr int numRkDir = 4;
 
-constexpr int KnDir[numKnDir] = {-8, -19, -21, -12, 8, 19, 21, 12};
+constexpr int KnDir[numKnDir] = {-8, -19, -21, -12, 8,  19,  21,  12};
+constexpr int RkDir[numRkDir] = {-1, 1, -10, 10};
 
 bool isAttackedByPawn(int sq, int side, Board& board) {
     if (side == WHITE) {
@@ -25,11 +27,32 @@ bool isAttackedByKnight(int sq, int side, Board& board) {
     }
     return false;
 }
+bool isAttackedByRookQueen(int sq, int side, Board& board) {
+    for (int index = 0; index < numRkDir; index++) {
+        int newSq = sq + RkDir[index];
+        int curPiece = board.squareToPiece[newSq];
+
+        while (curPiece != OFFBOARD) {
+            if (curPiece != EMPTY) {
+                if (isRookQueen[curPiece] && pieceColor[curPiece] == side) {
+                    return true;
+                }
+                break;
+            } 
+            newSq += RkDir[index];
+            curPiece = board.squareToPiece[newSq];
+        }
+    }
+
+    return false;
+}
 
 bool isSqBeingAttacked(int sq, int side, Board &board) {
     assert(isSqOnBoard(sq));
     assert(isSideValid(side));
     assert(board.checkBoard());
 
-    return isAttackedByPawn(sq, side, board) || isAttackedByKnight(sq, side, board);
+    return isAttackedByPawn(sq, side, board) ||
+           isAttackedByKnight(sq, side, board) ||
+           isAttackedByRookQueen(sq, side, board);
 }
