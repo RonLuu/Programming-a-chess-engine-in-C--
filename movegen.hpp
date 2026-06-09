5b53757244792424dc1e8605023e1329a7e882aa
trieukhail <trieukhail@student.unimelb.edu.au>
2026-06-09 09:39:58 +0700

    Add makeMove constexpr

--- movegen.hpp
+++ movegen.hpp
@@ -1,3 +1,8 @@
 #include "board.hpp"
+#include "attack.hpp"
+
+constexpr int makeMove(int from, int to, int captured, int promoted, int FLAG) {
+    return from | (to << 7) | (captured << 14) | (promoted << 20) | FLAG;
+}
 
 void generateAllMoves(Board& board, MOVELIST& moveList);
