fa052dfc27caec234af1cfa446b427f9869aaf64
trieukhail <trieukhail@student.unimelb.edu.au>
2026-06-09 19:53:28 +0700

    Define makemove.hpp

---
+++ makemove.hpp
@@ -0,0 +1,29 @@
+#include "board.hpp"
+
+constexpr int castlePermission[120] = {
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+    15, 13, 15, 15, 15, 12, 15, 15, 14, 15,
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+    15,  7, 15, 15, 15,  3, 15, 15, 11, 15,
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+};
+
+inline void hashPiece(int piece, int sq, Board &board) {
+    board.hashkey ^= pieceHashKeys[piece][sq];
+}
+inline void hashCastle(int castlePerm, Board &board) {
+    board.hashkey ^= castleHashKeys[castlePerm];
+}
+inline void hashSide(Board &board) {
+    board.hashkey ^= sideHashKey;
+}
+inline void hashEnPassant(Board &board) {
+    board.hashkey ^= pieceHashKeys[EMPTY][board.enPasSq];
+}
