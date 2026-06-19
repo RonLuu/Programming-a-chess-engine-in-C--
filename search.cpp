2107ee80e90f5193b22f7af0273f2352eac1101d
trieukhail <trieukhail@student.unimelb.edu.au>
2026-06-19 08:36:49 +0700

    Add assert line

--- search.cpp
+++ search.cpp
@@ -3,7 +3,8 @@
 bool isBoardRepeated(Board &board) {
     // Performance trick: start serach from the last fifty move was set
     for (int index = board.historyIndex - board.fiftyMove;
-         index < board.historyIndex - 1; index++) {
+        index < board.historyIndex - 1; index++) {
+        assert(index >= 0 && index <= MAX_GAME_MOVE);
         if (board.hashkey == board.history[index].hashkey) {
             return true;
         }
